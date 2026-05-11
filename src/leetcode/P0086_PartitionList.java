dac21de56ec9c4b17db5b92cf077f760b352182d
dawn <3255235912@qq.com>
2026-05-11 21:37:41 +0800

    feat: 添加链表分隔功能，按指定值分隔节点并保留相对位置

---
+++ src/leetcode/P0086_PartitionList.java
@@ -0,0 +1,83 @@
+package leetcode;
+
+import leetcode.common.ListNode;
+
+/**
+ * <p>给你一个链表的头节点 <code>head</code> 和一个特定值 <code>x</code>，请你对链表进行分隔，使得所有 <strong>小于</strong> <code>x</code> 的节点都出现在 <strong>大于或等于</strong> <code>x</code> 的节点之前。</p>
+ *
+ * <p>你应当 <strong>保留</strong> 两个分区中每个节点的初始相对位置。</p>
+ *
+ * <p><strong>示例 1：</strong></p>
+ * <pre>
+ * <strong>输入：</strong>head = [1,4,3,2,5,2], x = 3
+ * <strong>输出：</strong>[1,2,2,4,3,5]
+ * </pre>
+ *
+ * <p><strong>示例 2：</strong></p>
+ * <pre>
+ * <strong>输入：</strong>head = [2,1], x = 2
+ * <strong>输出：</strong>[1,2]
+ * </pre>
+ *
+ * <p><strong>提示：</strong></p>
+ * <ul>
+ * <li>链表中节点的数目在范围 <code>[0, 200]</code> 内</li>
+ * <li><code>-100 &lt;= Node.val &lt;= 100</code></li>
+ * <li><code>-200 &lt;= x &lt;= 200</code></li>
+ * </ul>
+ */
+public class P0086_PartitionList {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(1,
+                new ListNode(4,
+                        new ListNode(3,
+                                new ListNode(2,
+                                        new ListNode(5,
+                                                new ListNode(2))))));
+        ListNode result = solution.partition(head, 3);
+        printList(result);
+    }
+
+    private static void printList(ListNode head) {
+        StringBuilder builder = new StringBuilder();
+        builder.append('[');
+        while (head != null) {
+            builder.append(head.val);
+            head = head.next;
+            if (head != null) {
+                builder.append(',');
+            }
+        }
+        builder.append(']');
+        System.out.println(builder);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    static class Solution {
+        public ListNode partition(ListNode head, int x) {
+            ListNode leftDummy = new ListNode(0);
+            ListNode rightDummy = new ListNode(0);
+            ListNode leftTail = leftDummy;
+            ListNode rightTail = rightDummy;
+
+            while (head != null) {
+                ListNode next = head.next;
+                head.next = null;
+                if (head.val < x) {
+                    leftTail.next = head;
+                    leftTail = head;
+                } else {
+                    rightTail.next = head;
+                    rightTail = head;
+                }
+                head = next;
+            }
+
+            leftTail.next = rightDummy.next;
+            return leftDummy.next;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
+
