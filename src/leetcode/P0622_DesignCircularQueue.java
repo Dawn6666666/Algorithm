148bc84a89fcedf78001acc552d18a3a59960072
dawn <3255235912@qq.com>
2026-05-12 21:26:16 +0800

    feat: 优化循环队列实现，修正入队和出队逻辑，更新队首和队尾获取方法

--- src/leetcode/P0622_DesignCircularQueue.java
+++ src/leetcode/P0622_DesignCircularQueue.java
@@ -7,52 +7,72 @@ public class P0622_DesignCircularQueue {
     public static void main(String[] args) {
         MyCircularQueue circularQueue = new MyCircularQueue(3);
         System.out.println(circularQueue.enQueue(1));
+        System.out.println(circularQueue.Front());
         System.out.println(circularQueue.enQueue(2));
         System.out.println(circularQueue.enQueue(3));
         System.out.println(circularQueue.enQueue(4));
         System.out.println(circularQueue.Rear());
         System.out.println(circularQueue.isFull());
         System.out.println(circularQueue.deQueue());
+        System.out.println(circularQueue.Front());
         System.out.println(circularQueue.enQueue(4));
         System.out.println(circularQueue.Rear());
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     static class MyCircularQueue {
-        private final int[] data;
-        private int front;
-        private int rear;
+        private final int[] queue;
+        private int l;
+        private int r;
         private int size;
+        private int limit;
 
         public MyCircularQueue(int k) {
-            data = new int[k];
+            queue = new int[k];
+            l = r = size = 0;
+            limit = k;
         }
 
         public boolean enQueue(int value) {
             if (isFull()) {
                 return false;
             }
-            data[rear] = value;
-            rear = (rear + 1) % data.length;
-            size++;
-            return true;
+            else {
+                queue[r] = value;
+                r = r == (limit - 1) ? 0 : r + 1;
+                size++;
+                return true;
+            }
         }
 
         public boolean deQueue() {
             if (isEmpty()) {
                 return false;
             }
-            front = (front + 1) % data.length;
-            size--;
-            return true;
+            else {
+                l = l == (limit - 1) ? 0 : l + 1;
+                size--;
+                return true;
+            }
         }
 
         public int Front() {
-            return isEmpty() ? -1 : data[front];
+            if (isEmpty()) {
+                return -1;
+            }
+            else {
+                return queue[l];
+            }
         }
 
         public int Rear() {
-            return isEmpty() ? -1 : data[(rear - 1 + data.length) % data.length];
+            if (isEmpty()) {
+                return -1;
+            }
+            else {
+                int tail = r == 0 ? (limit - 1) : (r - 1);
+                return queue[tail];
+            }
         }
 
         public boolean isEmpty() {
@@ -60,9 +80,11 @@ public class P0622_DesignCircularQueue {
         }
 
         public boolean isFull() {
-            return size == data.length;
+            return size == limit;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
+
+
 }
 
